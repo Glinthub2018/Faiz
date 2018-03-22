e16201fca8d13387ecf4a240c564dfe65c67ba24
FAIZ <faiz.undefined@gmail.com>
2018-03-22 14:29:05 -0500

    Initial Commit

---
+++ HairCutteryClub2018/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  HairCutteryClub2018
+//
+//  Created by Faizuddin Shaik on 3/22/18.
+//  Copyright © 2018 GlintHub. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
